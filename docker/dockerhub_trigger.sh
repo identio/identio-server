e3684f1599b3c4ba7d9d8093fdb4c9387364c6db
Loeiz Tanguy <loeiz.tanguy@gmail.com>
2016-08-21 11:42:40 +0200

    Improvement to the Docker Hub integration
    - Curl now executes silently
    - Two builds for each release: one tagged latest and one for the pushed tag

--- docker/dockerhub_trigger.sh
+++ docker/dockerhub_trigger.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-curl -H "Content-Type\: application/json" --data '{"source_type": "Branch", "source_name": "master"}' -X POST $DOCKER_HUB_TRIGGER_URL
+curl -s -H "Content-Type\: application/json" --data '{"source_type": "Branch", "source_name": "master"}' -X POST $DOCKER_HUB_TRIGGER_URL
+curl -s -H "Content-Type: application/json" --data '{"source_type": "Tag", "source_name": "'"$TRAVIS_TAG"'"}' -X POST $DOCKER_HUB_TRIGGER_URL
