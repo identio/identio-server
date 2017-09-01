4c68d8675f457339a7c6959eebe0f9e09631a581
Loeiz TANGUY <loeiz.tanguy@gmail.com>
2017-09-01 12:17:11 +0200

    Arrange steps of integration test in separate methods

---
+++ src/test/java/net/identio/server/tests/integ/oauth/ImplicitRequests.java
@@ -0,0 +1,223 @@
+/*
+ * This file is part of Ident.io.
+ *
+ * Ident.io - A flexible authentication server
+ * Copyright (c) 2017 Loeiz TANGUY
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package net.identio.server.tests.integ.oauth;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import net.identio.server.model.AuthorizationScope;
+import net.identio.server.mvc.common.model.ApiResponseStatus;
+import net.identio.server.mvc.common.model.AuthMethodResponse;
+import net.identio.server.mvc.common.model.AuthSubmitRequest;
+import net.identio.server.mvc.common.model.AuthSubmitResponse;
+import net.identio.server.mvc.oauth.model.ConsentContext;
+import net.identio.server.mvc.oauth.model.ConsentRequest;
+import net.identio.server.mvc.oauth.model.ConsentResponse;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+
+import java.io.FileInputStream;
+import java.security.KeyStore;
+import java.security.interfaces.RSAKey;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+
+class ImplicitRequests {
+
+    private static final String AUTHENTICATION_URL = "/#!/auth/";
+
+    private int port;
+    private TestRestTemplate restTemplate;
+
+    private HttpHeaders headers;
+    private String responseUrl;
+
+    ImplicitRequests(int port, TestRestTemplate restTemplate) {
+        this.port = port;
+        this.restTemplate = restTemplate;
+    }
+
+    void authorizeRequest() {
+
+        ResponseEntity<String> initialRequestResponse = restTemplate.exchange(
+                "/oauth/authorize?client_id=test&redirect_uri=http://example.com/cb&response_type=token&scope=scope.test.1 scope.test.2&state=1234",
+                HttpMethod.GET,
+                new HttpEntity<>(null, new HttpHeaders()),
+                String.class);
+
+        // Redirect to the login page
+        String redirectUrl = initialRequestResponse.getHeaders().getFirst(HttpHeaders.LOCATION);
+
+        assertEquals(HttpStatus.FOUND, initialRequestResponse.getStatusCode());
+        assertTrue(redirectUrl.startsWith(getUrlWithPort(AUTHENTICATION_URL)));
+
+        String sessionCookie = getSessionCookie(initialRequestResponse);
+        String transactionId = getTransactionId(redirectUrl);
+
+        assertTrue(sessionCookie.startsWith("identioSession="));
+        assertNotNull(transactionId);
+
+        // Request authentication methods
+        headers = new HttpHeaders();
+        headers.add(HttpHeaders.COOKIE, sessionCookie);
+        headers.add("X-Transaction-ID", transactionId);
+    }
+
+    void getAuthMethods() {
+
+        ResponseEntity<AuthMethodResponse[]> authMethodResponse = restTemplate.exchange(
+                "/api/auth/methods",
+                HttpMethod.GET,
+                new HttpEntity<>(null, headers),
+                AuthMethodResponse[].class);
+
+        assertEquals(HttpStatus.OK, authMethodResponse.getStatusCode());
+        assertEquals(1, authMethodResponse.getBody().length);
+        assertEquals("Local", authMethodResponse.getBody()[0].getName());
+        assertEquals("local", authMethodResponse.getBody()[0].getType());
+
+    }
+
+    void authenticateLocal() {
+
+        AuthSubmitRequest authenticationSubmit = new AuthSubmitRequest().setLogin("johndoe").setPassword("password")
+                .setMethod("Local");
+
+        ResponseEntity<AuthSubmitResponse> authSubmitResponseEntity = restTemplate.exchange(
+                "/api/auth/submit/password",
+                HttpMethod.POST,
+                new HttpEntity<>(authenticationSubmit, headers),
+                AuthSubmitResponse.class);
+
+        // Check that the authentication is successful and that we're asked for consent
+        AuthSubmitResponse authSubmitResponse = authSubmitResponseEntity.getBody();
+
+        assertEquals(HttpStatus.OK, authSubmitResponseEntity.getStatusCode());
+        assertEquals(ApiResponseStatus.CONSENT, authSubmitResponse.getStatus());
+    }
+
+    void getConsentContext() {
+
+        // Get information for consent screen
+        ResponseEntity<ConsentContext> consentContextEntity = restTemplate.exchange(
+                "/api/authz/consent",
+                HttpMethod.GET,
+                new HttpEntity<>(null, headers),
+                ConsentContext.class);
+
+        ConsentContext consentContext = consentContextEntity.getBody();
+
+        assertEquals("Test Client", consentContext.getAudience());
+
+        List<AuthorizationScope> requestedScopes = consentContext.getRequestedScopes();
+        requestedScopes.sort(Comparator.comparing(AuthorizationScope::getName));
+
+        assertTrue(consentContext.getRequestedScopes().size() == 2);
+
+        assertEquals("scope.test.1", consentContext.getRequestedScopes().get(0).getName());
+        assertEquals("Accéder à scope test 1", consentContext.getRequestedScopes().get(0).getDescription().get("fr"));
+        assertEquals("Access scope test 1", consentContext.getRequestedScopes().get(0).getDescription().get("en"));
+        assertEquals("scope.test.2", consentContext.getRequestedScopes().get(1).getName());
+    }
+
+    void consent() {
+
+        // Send the consent
+        ConsentRequest consentRequest = new ConsentRequest().setApprovedScopes(Collections.singletonList("scope.test.1"));
+
+        ResponseEntity<ConsentResponse> consentResponseEntity = restTemplate.exchange(
+                "/api/authz/consent",
+                HttpMethod.POST,
+                new HttpEntity<>(consentRequest, headers),
+                ConsentResponse.class);
+
+        ConsentResponse consentResponse = consentResponseEntity.getBody();
+        this.responseUrl = consentResponse.getResponseData().getUrl();
+
+        assertTrue(this.responseUrl
+                .matches("^http://example.com/cb#expires_in=2400&token_type=Bearer&access_token=.*&state=1234"));
+    }
+
+    void validateResponse() {
+        // Parse and validate JWT
+        Algorithm algorithm = null;
+        try {
+            algorithm = Algorithm.RSA256(getPublicSigningKey());
+        } catch (Exception e) {
+            fail();
+        }
+
+        JWTVerifier verifier = JWT.require(algorithm)
+                .withIssuer("https://localhost")
+                .withSubject("johndoe")
+                .withAudience("Test Client")
+                .withClaim("scope", "scope.test.1")
+                .build();
+
+        Pattern pattern = Pattern.compile("^http://example.com/cb#expires_in=2400&token_type=Bearer&access_token=(.*)&state=1234");
+        Matcher matcher = pattern.matcher(this.responseUrl);
+
+        if (matcher.find()) {
+            verifier.verify(matcher.group(1));
+        } else {
+            fail();
+        }
+    }
+
+    private String getUrlWithPort(String url) {
+
+        return "http://localhost:" + port + url;
+    }
+
+    private String getSessionCookie(ResponseEntity<?> response) {
+
+        return response.getHeaders().getFirst(HttpHeaders.SET_COOKIE);
+    }
+
+    private String getTransactionId(String url) {
+
+        return url.substring(getUrlWithPort(AUTHENTICATION_URL).length());
+    }
+
+    private RSAKey getPublicSigningKey() throws Exception {
+
+        FileInputStream fis = new FileInputStream(
+                "src/test/resources/oauth-server-config/default-sign-certificate.p12");
+
+        KeyStore ks = KeyStore.getInstance("PKCS12");
+        ks.load(fis, "password".toCharArray());
+
+        Enumeration<String> aliases = ks.aliases();
+
+        String alias = aliases.nextElement();
+
+        return (RSAKey) (ks.getCertificate(alias)).getPublicKey();
+
+    }
+}
--- src/test/java/net/identio/server/tests/integ/oauth/OAuthImplicitFullCinematicTest.java
+++ src/test/java/net/identio/server/tests/integ/oauth/OAuthImplicitFullCinematicTest.java
@@ -21,36 +21,15 @@
 
 package net.identio.server.tests.integ.oauth;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
 import net.identio.server.boot.IdentioServerApplication;
-import net.identio.server.model.AuthorizationScope;
-import net.identio.server.mvc.common.model.ApiResponseStatus;
-import net.identio.server.mvc.common.model.AuthMethodResponse;
-import net.identio.server.mvc.common.model.AuthSubmitRequest;
-import net.identio.server.mvc.common.model.AuthSubmitResponse;
-import net.identio.server.mvc.oauth.model.ConsentContext;
-import net.identio.server.mvc.oauth.model.ConsentRequest;
-import net.identio.server.mvc.oauth.model.ConsentResponse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.*;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.io.FileInputStream;
-import java.security.*;
-import java.security.interfaces.RSAKey;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
@@ -65,152 +44,21 @@ public class OAuthImplicitFullCinematicTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    private static final String AUTHENTICATION_URL = "/#!/auth/";
-
     @Test
     public void oAuthAuthorizeValidRequest() {
 
-        ResponseEntity<String> initialRequestResponse = restTemplate.exchange(
-                "/oauth/authorize?client_id=test&redirect_uri=http://example.com/cb&response_type=token&scope=scope.test.1 scope.test.2&state=1234",
-                HttpMethod.GET,
-                new HttpEntity<>(null, new HttpHeaders()),
-                String.class);
+        ImplicitRequests requests = new ImplicitRequests(port, restTemplate);
 
-        // Redirect to the login page
-        String redirectUrl = initialRequestResponse.getHeaders().getFirst(HttpHeaders.LOCATION);
+        requests.authorizeRequest();
 
-        assertEquals(HttpStatus.FOUND, initialRequestResponse.getStatusCode());
-        assertTrue(redirectUrl.startsWith(getUrlWithPort(AUTHENTICATION_URL)));
+        requests.getAuthMethods();
 
+        requests.authenticateLocal();
 
-        // Extract session cookie and transactionId
-        String sessionIdCookie = getSessionCookie(initialRequestResponse);
-        String transactionId = getTransactionId(redirectUrl);
+        requests.getConsentContext();
 
-        assertTrue(sessionIdCookie.startsWith("identioSession="));
-        assertNotNull(transactionId);
+        requests.consent();
 
-        // Request authentication methods
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.COOKIE, sessionIdCookie);
-        headers.add("X-Transaction-ID", transactionId);
-
-        ResponseEntity<AuthMethodResponse[]> authMethodResponse = restTemplate.exchange(
-                "/api/auth/methods",
-                HttpMethod.GET,
-                new HttpEntity<>(null, headers),
-                AuthMethodResponse[].class);
-
-        assertEquals(HttpStatus.OK, authMethodResponse.getStatusCode());
-        assertEquals(1, authMethodResponse.getBody().length);
-        assertEquals("Local", authMethodResponse.getBody()[0].getName());
-        assertEquals("local", authMethodResponse.getBody()[0].getType());
-
-        // Authenticate with local method
-        AuthSubmitRequest authenticationSubmit = new AuthSubmitRequest().setLogin("johndoe").setPassword("password")
-                .setMethod("Local");
-
-        ResponseEntity<AuthSubmitResponse> authSubmitResponseEntity = restTemplate.exchange(
-                "/api/auth/submit/password",
-                HttpMethod.POST,
-                new HttpEntity<>(authenticationSubmit, headers),
-                AuthSubmitResponse.class);
-
-        // Check that the authentication is successful and that we're asked for consent
-        AuthSubmitResponse authSubmitResponse = authSubmitResponseEntity.getBody();
-
-        assertEquals(HttpStatus.OK, authSubmitResponseEntity.getStatusCode());
-        assertEquals(ApiResponseStatus.CONSENT, authSubmitResponse.getStatus());
-
-        // Get information for consent screen
-        ResponseEntity<ConsentContext> consentContextEntity = restTemplate.exchange(
-                "/api/authz/consent",
-                HttpMethod.GET,
-                new HttpEntity<>(null, headers),
-                ConsentContext.class);
-
-        ConsentContext consentContext = consentContextEntity.getBody();
-
-        assertEquals("Test Client", consentContext.getAudience());
-
-        List<AuthorizationScope> requestedScopes = consentContext.getRequestedScopes();
-        requestedScopes.sort(Comparator.comparing(AuthorizationScope::getName));
-
-        assertTrue(consentContext.getRequestedScopes().size() == 2);
-
-        assertEquals("scope.test.1", consentContext.getRequestedScopes().get(0).getName());
-        assertEquals("Accéder à scope test 1", consentContext.getRequestedScopes().get(0).getDescription().get("fr"));
-        assertEquals("Access scope test 1", consentContext.getRequestedScopes().get(0).getDescription().get("en"));
-        assertEquals("scope.test.2", consentContext.getRequestedScopes().get(1).getName());
-
-        // Send the consent
-        ConsentRequest consentRequest = new ConsentRequest().setApprovedScopes(Collections.singletonList("scope.test.1"));
-
-        ResponseEntity<ConsentResponse> consentResponseEntity = restTemplate.exchange(
-                "/api/authz/consent",
-                HttpMethod.POST,
-                new HttpEntity<>(consentRequest, headers),
-                ConsentResponse.class);
-
-        ConsentResponse consentResponse = consentResponseEntity.getBody();
-
-        assertTrue(consentResponse.getResponseData().getUrl()
-                .matches("^http://example.com/cb#expires_in=2400&token_type=Bearer&access_token=.*&state=1234"));
-
-        // Parse and validate JWT
-        Algorithm algorithm = null;
-        try {
-            algorithm = Algorithm.RSA256(getPublicSigningKey());
-        } catch (Exception e) {
-            fail();
-        }
-
-        JWTVerifier verifier = JWT.require(algorithm)
-                .withIssuer("https://localhost")
-                .withSubject("johndoe")
-                .withAudience("Test Client")
-                .withClaim("scope", "scope.test.1")
-                .build();
-
-        Pattern pattern = Pattern.compile("^http://example.com/cb#expires_in=2400&token_type=Bearer&access_token=(.*)&state=1234");
-        Matcher matcher = pattern.matcher(consentResponse.getResponseData().getUrl());
-
-        if (matcher.find()) {
-            verifier.verify(matcher.group(1));
-        } else {
-            fail();
-        }
-    }
-
-    private RSAKey getPublicSigningKey() throws Exception {
-
-        FileInputStream fis = new FileInputStream(
-                "src/test/resources/oauth-server-config/default-sign-certificate.p12");
-
-        KeyStore ks = KeyStore.getInstance("PKCS12");
-        ks.load(fis, "password".toCharArray());
-
-        Enumeration<String> aliases = ks.aliases();
-
-        String alias = aliases.nextElement();
-
-        return (RSAKey) (ks.getCertificate(alias)).getPublicKey();
-
-    }
-
-
-    private String getUrlWithPort(String url) {
-
-        return "http://localhost:" + port + url;
-    }
-
-    private String getSessionCookie(ResponseEntity<?> response) {
-
-        return response.getHeaders().getFirst(HttpHeaders.SET_COOKIE);
-    }
-
-    private String getTransactionId(String url) {
-
-        return url.substring(getUrlWithPort(AUTHENTICATION_URL).length());
+        requests.validateResponse();
     }
 }
