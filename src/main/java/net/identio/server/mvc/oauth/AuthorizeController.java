3b7ab9842d693a79196d15f2d077892f0014473a
Loeiz TANGUY <loeiz.tanguy@gmail.com>
2017-11-23 11:38:44 +0100

    Make the authorize endpoint more compliant with the spec by rejecting parameters that are present multiple times

--- src/main/java/net/identio/server/mvc/oauth/AuthorizeController.java
+++ src/main/java/net/identio/server/mvc/oauth/AuthorizeController.java
@@ -20,6 +20,8 @@
  */
 package net.identio.server.mvc.oauth;
 
+import net.identio.server.model.Result;
+import net.identio.server.service.oauth.model.OAuthErrors;
 import net.identio.server.service.orchestration.exceptions.ServerException;
 import net.identio.server.service.orchestration.exceptions.ValidationException;
 import net.identio.server.service.orchestration.exceptions.WebSecurityException;
@@ -27,10 +29,12 @@ import net.identio.server.model.OAuthInboundRequest;
 import net.identio.server.mvc.common.TransparentAuthController;
 import net.identio.server.service.orchestration.RequestOrchestrationService;
 import net.identio.server.service.orchestration.model.RequestValidationResult;
+import net.identio.server.utils.HttpUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -49,23 +53,37 @@ public class AuthorizeController {
 
     @RequestMapping(value = "/oauth/authorize", method = RequestMethod.GET)
     public String authorizeRequest(
-            @RequestParam(value = "response_type", required = false) String responseType,
-            @RequestParam(value = "client_id", required = false) String clientId,
-            @RequestParam(value = "redirect_uri", required = false) String redirectUri,
-            @RequestParam(value = "scope", required = false) String scopes,
-            @RequestParam(value = "state", required = false) String state,
-            @RequestParam(value = "code_challenge", required = false) String codeChallenge,
-            @RequestParam(value = "code_challenge_method", required = false) String codeChallengeMethod,
+            @RequestParam MultiValueMap<String, String> allParams,
             @CookieValue(required = false) String identioSession,
             HttpServletRequest httpRequest,
             HttpServletResponse httpResponse) throws ValidationException, ServerException, WebSecurityException {
 
+        Result<String> responseType = HttpUtils.getUniqueParam(allParams, "response_type");
+        Result<String> clientId = HttpUtils.getUniqueParam(allParams, "client_id");
+        Result<String> redirectUri = HttpUtils.getUniqueParam(allParams, "redirect_uri");
+        Result<String> scopes = HttpUtils.getUniqueParam(allParams, "scope");
+        Result<String> state = HttpUtils.getUniqueParam(allParams, "state");
+        Result<String> codeChallenge = HttpUtils.getUniqueParam(allParams, "code_challenge");
+        Result<String> codeChallengeMethod = HttpUtils.getUniqueParam(allParams, "code_challenge_method");
+
+        if (!responseType.isSuccess() ||
+                !clientId.isSuccess() ||
+                !redirectUri.isSuccess() ||
+                !scopes.isSuccess() ||
+                !state.isSuccess() ||
+                !codeChallenge.isSuccess() ||
+                !codeChallengeMethod.isSuccess()
+                )
+            return "redirect:/#!/error/" + OAuthErrors.INVALID_REQUEST;
+
+
         LOG.info("Received OAuth authorization request from ClientId: {}", clientId);
         LOG.debug("RT: {} - RU: {} - SC: {} - ST: {}", responseType, redirectUri, scopes, state);
 
-        OAuthInboundRequest request = new OAuthInboundRequest().setClientId(clientId).setResponseType(responseType)
-                .setRedirectUri(redirectUri).setScope(scopes).setState(state).setCodeChallenge(codeChallenge)
-                .setCodeChallengeMethod(codeChallengeMethod);
+
+        OAuthInboundRequest request = new OAuthInboundRequest().setClientId(clientId.get()).setResponseType(responseType.get())
+                .setRedirectUri(redirectUri.get()).setScope(scopes.get()).setState(state.get())
+                .setCodeChallenge(codeChallenge.get()).setCodeChallengeMethod(codeChallengeMethod.get());
 
         RequestValidationResult result = validationService.validateRequest(request, identioSession);
 
